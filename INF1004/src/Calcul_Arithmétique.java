958d2e98ddc5c9cd7d8d68793233f95d806b7ac4
harlez <harlezloulelo@gmail.com>
2020-07-14 07:53:13 -0400

    codage de la classe arithmétique.

---
+++ INF1004/src/Calcul_Arithmétique.java
@@ -0,0 +1,27 @@
+public class Calcul_Arithmétique {
+
+    public double Addition (double a, double b)
+    {
+        return  a + b;
+    }
+
+    public double Soustraction (double a, double b)
+    {
+        return a - b;
+    }
+
+    public double Multiplication (double a, double b)
+    {
+        return a * b;
+    }
+
+    public double Division (double a, double b)
+    {
+        return a / b;
+    }
+
+    public double Racine_carrée (double c)
+    {
+        return Math.sqrt(c);
+    }
+}
